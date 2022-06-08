125af462d9c13a56ffd2ef1016198e706e51bb00
Tingweiftw <tingwei.f@gmail.com>
2022-06-08 22:20:14 +0800

    unicafe step2

--- part1_materials/unicafe/src/App.js
+++ part1_materials/unicafe/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Display = ({label, counter }) => {
+    return (
+        <div>{label}: {counter}</div>
+    )
 }
 
-export default App;
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const voteGood = () => setGood(good+1)
+  const voteNeutral = () => setNeutral(neutral+1)
+  const voteBad = () => setBad(bad+1)
+  const sumAll = () => good+bad+neutral
+  const averageAll = () => (good*1+bad*-1+neutral*0) / sumAll()
+  const positivePct = () => good / sumAll() * 100
+
+  return (
+    <div>
+        <h1>Give Feedback</h1>
+        <Button onClick={voteGood} text={"Good"}/>
+        <Button onClick={voteNeutral} text={"Neutral"}/>
+        <Button onClick={voteBad} text={"Bad"}/>
+        <h1>Statistic</h1>
+        <Display label = {"Good"} counter = {good}/>
+        <Display label = {"Neutral"} counter = {neutral}/>
+        <Display label = {"Bad"} counter = {bad}/>
+        <Display label = {"All"} counter = {sumAll()}/>
+        <Display label = {"Average"} counter = {averageAll()}/>
+        <Display label = {"Positive"} counter = {positivePct() + "%"}/>
+
+    </div>
+  )
+}
+
+export default App
