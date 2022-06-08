9da88c8b6e2dbb21c96048c56f8349154dc68dbe
Tingweiftw <tingwei.f@gmail.com>
2022-06-08 18:54:14 +0800

    exercise 1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p> Hello {props.name}, you are {props.age} years old</p>
     </div>
-  );
+  )
 }
 
-export default App;
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://tingwei.fans">Ting Wei</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const now = new Date()
+  const age = 10 
+  const name = "John" 
+  return (
+  <div>
+    <h1> Greetings </h1>
+    <Hello name = "George" age = {25} />
+    <Hello name = {name} age = {age}/>
+    <Footer />
+  </div>
+  )
+}
+
+export default App
