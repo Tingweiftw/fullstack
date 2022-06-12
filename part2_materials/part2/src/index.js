7f6fd87a3e907863b7b7966601def18bd1b63d4e
Fan Ting Wei <tingwei.f@gmail.com>
2022-06-13 00:19:47 +0800

    removed unnecessary files in part 2

--- part2_materials/part2/src/index.js
+++ part2_materials/part2/src/index.js
@@ -1,14 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import axios from 'axios'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App/>
-)
-
-axios
-  .get('http://localhost:3001/notes')
-  .then(response =>{
-    const notes = response.data
-})
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
--- part2_materials/phonebook/src/component/Persons.js
+++ part2_materials/phonebook/src/component/Persons.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 const Contact = ({person}) => {
     return (
       <div>{person.name} {person.number}</div>
