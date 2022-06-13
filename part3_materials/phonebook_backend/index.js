59a18dc307fee77488ca9781eb5474da9fc9a3a2
Tingweiftw <tingwei.f@gmail.com>
2022-06-14 01:30:51 +0800

    part3 heroku

--- part3_materials/phonebook_backend/index.js
+++ part3_materials/phonebook_backend/index.js
@@ -1,10 +1,12 @@
 const { response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 let persons = [
     {
